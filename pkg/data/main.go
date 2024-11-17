4f880bc0a8c733457a331ec7381210955e59ff99
Alexander Utterodt <alexander.utterodt@gmail.com>
2024-11-17 14:39:02 +0100

    Added simple loader for the syntheticdataset.dat format

--- pkg/data/main.go
+++ pkg/data/main.go
@@ -1,6 +1,15 @@
 package data
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"path"
+	"strings"
+)
+
+var typeAssociationMap = map[string]FileParser{
+	".dat": DefaultDatParser(),
+}
 
 // Import data from a file
 func Import(data []byte, filename string) error {
@@ -8,5 +17,17 @@ func Import(data []byte, filename string) error {
 
 	fmt.Printf("Importierte Datei: %s, Größe: %d bytes\n", filename, len(data))
 
+	dataType := strings.ToLower(path.Ext(filename))
+	parser, ok := typeAssociationMap[dataType]
+	if !ok {
+		return errors.New(fmt.Sprintf("Can't import %s: No loader for %s files found", filename, dataType))
+	} else {
+		res, err := parser.tryParse(data)
+		if err != nil {
+			return errors.Join(errors.New(fmt.Sprintf("Import failed (%s)", filename)), err)
+		}
+		//TODO display loaded content
+		fmt.Println(res)
+	}
 	return nil
 }
