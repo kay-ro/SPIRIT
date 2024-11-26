0c5481f2b33deb8f0424eed24ca51130e42ad3b0
Alexander Utterodt <alexander.utterodt@stud.tu-darmstdt.de>
2024-11-26 12:59:31 +0100

    WIP: Fixed getEden and added function doc on NewOldSLDFunction

--- pkg/data/old_function.go
+++ pkg/data/old_function.go
@@ -2,26 +2,29 @@ package data
 
 import "math"
 
+// NewOldSLDFunction returns a DataPoint function Based on the old implementation of the getEden function
+//
+// - eden is an array with all the eden values {eden_a,eden_1,eden_2,...,eden_n,eden_b}
+//
+// - d array with the d values {d_1,d_2,...,d_n}
+//
+// - sigma array with sigma values {sigma_a1,sigma_12,sigma_23,...,sigma_(n-1)(n),sigma_nb}
 func NewOldSLDFunction(eden []float64, d []float64, sigma []float64, zNumber int) *DataFunction {
 	return NewDataFunction(getEden(eden, d, sigma, zNumber), INTERPOLATION_NONE)
 }
 
+// Eden_a varies so eden[0] is now eden_a
 func getEden(eden []float64, d []float64, sigma []float64, zNumber int) []Point { // TODO understand this mess?
 	zAxis := getZAxis(d, zNumber)
 	var z_a = make([]float64, len(d)+2)
 	z_a[0] = 0.0
 	for i := 1; i < len(d)+2; i++ {
-		if i < 4 {
-			z_a[i] = 0.0
+		if i == 4 {
+			z_a[i] = z_a[i-1] + 2*d[i-1]
 		} else {
-			z_a[i] = d[1]
-		}
-		for j := 0; j < i; j++ {
-			z_a[i] += d[j]
+			z_a[i] = z_a[i-1] + d[i-1]
 		}
 	}
-	edenA := 0.333
-	eden = append([]float64{edenA}, eden...)
 	resEden := make([]Point, zNumber)
 	for i := 0; i < zNumber; i++ {
 		z := zAxis[i]
