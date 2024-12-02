0de90a8ac212ced103e2d2712aa4941fb58bcc1f
Alexander Utterodt <alexander.utterodt@stud.tu-darmstdt.de>
2024-12-02 20:01:16 +0100

    Implemented old eden(3,7) functions

--- pkg/data/old_function.go
+++ pkg/data/old_function.go
@@ -1,6 +1,10 @@
 package data
 
-import "math"
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
 // NewOldSLDFunction returns a DataPoint function Based on the old implementation of the getEden function
 //
@@ -10,12 +14,132 @@ import "math"
 //
 // - sigma array with sigma values {sigma_a1,sigma_12,sigma_23,...,sigma_(n-1)(n),sigma_nb} (Roughness)
 //
+// # Returns nil, when old getEden was not implemented for parameter count
+//
+// **NOTE** This uses old 1 to 1 translated code from IDL and has not to be defined for any parameter count
+//
 // Deprecated: Use new function implementation when possible
 func NewOldSLDFunction(eden []float64, d []float64, sigma []float64, zNumber int) *DataFunction {
-	return NewDataFunction(getEden(eden, d, sigma, zNumber), INTERPOLATION_NONE)
+	points, err := getEden(eden, d, sigma, zNumber)
+	if err != nil {
+		return nil
+	}
+	return NewDataFunction(points, INTERPOLATION_NONE)
 }
 
-// getEden returns a DataPoints based on the old implementation of the getEden function
+var edens map[int]func(eden []float64, d []float64, sigma []float64, zNumber int) []Point
+
+func init() {
+	edens = make(map[int]func(eden []float64, d []float64, sigma []float64, zNumber int) []Point)
+	edens[3] = eden3
+	edens[7] = eden7
+}
+
+// get_eden3 from fit_refl_monolayer
+func eden3(edensitys []float64, d []float64, sigma []float64, znumber int) []Point {
+	zaxis := get_zaxis(d, znumber)
+
+	eden_a := edensitys[0] //old 0.0
+	eden_1 := edensitys[1]
+	eden_2 := edensitys[2]
+	eden_3 := edensitys[3]
+	eden_b := edensitys[4]
+	d_1 := d[0]
+	d_2 := d[1]
+	d_3 := d[2]
+	sigma_a1 := math.Abs(sigma[0])
+	sigma_12 := math.Abs(sigma[1])
+	sigma_23 := math.Abs(sigma[2])
+	sigma_3b := math.Abs(sigma[3])
+
+	z_a1 := 0.0
+	z_12 := d_1
+	z_23 := d_1 + d_2
+	z_3b := d_1 + d_2 + d_3
+
+	eden := make([]Point, znumber)
+	for i := 0; i < znumber; i++ {
+		z := zaxis[i]
+		step1 := (eden_1 - eden_a) * 0.5 * (1.0 + math.Erf((z-z_a1)/(math.Sqrt2*sigma_a1)))
+		step2 := (eden_2 - eden_1) * 0.5 * (1.0 + math.Erf((z-z_12)/(math.Sqrt2*sigma_12)))
+		step3 := (eden_3 - eden_2) * 0.5 * (1.0 + math.Erf((z-z_23)/(math.Sqrt2*sigma_23)))
+		step4 := (eden_b - eden_3) * 0.5 * (1.0 + math.Erf((z-z_3b)/(math.Sqrt2*sigma_3b)))
+		eden_i := step1 + step2 + step3 + step4
+		eden[i] = Point{
+			X:   z,
+			Y:   eden_i,
+			ERR: 0,
+		}
+	}
+
+	return eden
+}
+
+// get_eden from fit_bilayer_rigaku
+func eden7(edensitys []float64, d []float64, sigma []float64, zNumber int) []Point {
+
+	zaxis := get_zaxis(d, zNumber)
+
+	eden_a := edensitys[0] //old 0.33
+	eden_1 := edensitys[1]
+	eden_2 := edensitys[2]
+	eden_3 := edensitys[3]
+	eden_4 := edensitys[4]
+	eden_5 := edensitys[5]
+	eden_6 := edensitys[6]
+	eden_7 := edensitys[7]
+	eden_b := edensitys[8]
+	d_1 := d[0]
+	d_2 := d[1]
+	d_3 := d[2]
+	d_4 := d[3]
+	d_5 := d[4]
+	d_6 := d[5]
+	d_7 := d[6]
+	sigma_a1 := math.Abs(sigma[0])
+	sigma_12 := math.Abs(sigma[1])
+	sigma_23 := math.Abs(sigma[2])
+	sigma_34 := math.Abs(sigma[3])
+	sigma_45 := math.Abs(sigma[4])
+	sigma_56 := math.Abs(sigma[5])
+	sigma_67 := math.Abs(sigma[6])
+	sigma_7b := math.Abs(sigma[7])
+
+	z_a1 := 0.0
+	z_12 := d_1
+	z_2a3 := d_1 + d_2
+	z_32b := d_1 + d_2 + d_3
+	z_2b4 := d_1 + 2*d_2 + d_3
+	z_45 := d_1 + 2*d_2 + d_3 + d_4
+	z_56 := d_1 + 2*d_2 + d_3 + d_4 + d_5
+	z_67 := d_1 + 2*d_2 + d_3 + d_4 + d_5 + d_6
+	z_7b := d_1 + 2*d_2 + d_3 + d_4 + d_5 + d_6 + d_7
+
+	eden := make([]Point, zNumber)
+
+	for i := 0; i < zNumber; i++ {
+		z := zaxis[i]
+		step1 := (eden_1 - eden_a) * 0.5 * (1.0 + math.Erf((z-z_a1)/(math.Sqrt2*sigma_a1)))
+		step2 := (eden_2 - eden_1) * 0.5 * (1.0 + math.Erf((z-z_12)/(math.Sqrt2*sigma_12)))
+		step3a := (eden_3 - eden_2) * 0.5 * (1.0 + math.Erf((z-z_2a3)/(math.Sqrt2*sigma_23)))
+		step3b := (eden_2 - eden_3) * 0.5 * (1.0 + math.Erf((z-z_32b)/(math.Sqrt2*sigma_23)))
+		step4 := (eden_4 - eden_2) * 0.5 * (1.0 + math.Erf((z-z_2b4)/(math.Sqrt2*sigma_34)))
+		step5 := (eden_5 - eden_4) * 0.5 * (1.0 + math.Erf((z-z_45)/(math.Sqrt2*sigma_45)))
+		step6 := (eden_6 - eden_5) * 0.5 * (1.0 + math.Erf((z-z_56)/(math.Sqrt2*sigma_56)))
+		step7 := (eden_7 - eden_6) * 0.5 * (1.0 + math.Erf((z-z_67)/(math.Sqrt2*sigma_67)))
+		step8 := (eden_b - eden_7) * 0.5 * (1.0 + math.Erf((z-z_7b)/(math.Sqrt2*sigma_7b)))
+		eden_i := eden_a + step1 + step2 + step3a + step3b + step4 + step5 + step6 + step7 + step8
+		eden[i] = Point{
+			X:   z,
+			Y:   eden_i,
+			ERR: 0,
+		}
+	}
+
+	return eden
+}
+
+// getEden returns a DataPoints based on the old implementation of the old getEden function
 //
 // - eden is an array with all the eden values {eden_a,eden_1,eden_2,...,eden_n,eden_b} (edensity)
 //
@@ -23,35 +147,18 @@ func NewOldSLDFunction(eden []float64, d []float64, sigma []float64, zNumber int
 //
 // - sigma array with sigma values {sigma_a1,sigma_12,sigma_23,...,sigma_(n-1)(n),sigma_nb} (Roughness)
 //
-// **NOTE** uses the old implementation with double usage of d[1]
+// **NOTE** This uses old 1 to 1 translated code from IDL and has not to be defined for any parameter count
 //
 // Deprecated: Use new function implementation when possible
-func getEden(eden []float64, d []float64, sigma []float64, zNumber int) []Point { // TODO understand this mess?
-	zAxis := getZAxis(d, zNumber)
-	var z_a = make([]float64, len(d)+1) // TODO all bugged for other than monolayer
-	z_a[0] = 0.0
-	for i := 1; i < len(z_a); i++ {
-		if i == 4 {
-			z_a[i] = z_a[i-1] + 2*d[i-1]
-		} else {
-			z_a[i] = z_a[i-1] + d[i-1]
-		}
+func getEden(eden []float64, d []float64, sigma []float64, zNumber int) ([]Point, error) { // TODO understand this mess?
+	f := edens[len(d)]
+	if f == nil {
+		return nil, errors.New(fmt.Sprintf("No getEden schema defined for %d edens", len(d)))
 	}
-	resEden := make([]Point, zNumber)
-	for i := 0; i < zNumber; i++ {
-		z := zAxis[i]
-		var step float64 = 0
-		for j := 0; j < len(z_a); j++ {
-			step += (eden[j+1] - eden[j]) * 0.5 * (math.Erf((z - z_a[j]) / (math.Sqrt2 * sigma[j])))
-		}
-		resEden[i] = Point{
-			X: z,
-			Y: step,
-		}
-	}
-	return resEden
+
+	return f(eden, d, sigma, zNumber), nil
 }
-func getZAxis(d []float64, zNumber int) []float64 {
+func get_zaxis(d []float64, zNumber int) []float64 {
 	z0 := -20.0
 	var z1 = d[1]
 	for _, f := range d {
