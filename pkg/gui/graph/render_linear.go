eabc0e6304f5acdeb953fc9a7e53175d1b4bfaff
Alexander Utterodt <alexander.utterodt@gmail.com>
2025-01-24 10:28:50 +0100

    [KAN-30] Added label clipping to linear graph

--- pkg/gui/graph/render_linear.go
+++ pkg/gui/graph/render_linear.go
@@ -15,9 +15,16 @@ func (r *GraphRenderer) DrawGraphLinear(scope *function.Scope, points, iPoints f
 	availableWidth := r.size.Width - (1.5 * r.margin)
 	availableHeight := r.size.Height - (1.5 * r.margin)
 
+	//
+	order := int(math.Floor(math.Log10(math.Abs(scope.MaxY - scope.MinY))))
+	minX := floorInOrder(scope.MinX, order)
+	minY := floorInOrder(scope.MinY, order)
+	maxX := ceilInOrder(scope.MaxX, order)
+	maxY := ceilInOrder(scope.MaxY, order)
+
 	// complete range
-	xRange := math.Abs(scope.MaxX - scope.MinX)
-	yRange := math.Abs(scope.MaxY - scope.MinY)
+	xRange := math.Abs(maxX - minX)
+	yRange := math.Abs(maxY - minY)
 
 	oX, oY := float32(0), float32(0)
 
@@ -25,8 +32,8 @@ func (r *GraphRenderer) DrawGraphLinear(scope *function.Scope, points, iPoints f
 	if !isDataSet {
 		for i, point := range iPoints {
 			// scale x value to available width
-			x := float32((point.X-scope.MinX)/xRange) * availableWidth
-			y := float32((point.Y-scope.MinY)/yRange) * availableHeight
+			x := float32((point.X-minX)/xRange) * availableWidth
+			y := float32((point.Y-minY)/yRange) * availableHeight
 
 			// first point is the origin
 			if i == 0 {
@@ -51,14 +58,14 @@ func (r *GraphRenderer) DrawGraphLinear(scope *function.Scope, points, iPoints f
 	// draw data points
 	for _, point := range points {
 		// scale x value to available width
-		x := float32((point.X-scope.MinX)/xRange) * availableWidth
-		y := float32((point.Y-scope.MinY)/yRange) * availableHeight
+		x := float32((point.X-minX)/xRange) * availableWidth
+		y := float32((point.Y-minY)/yRange) * availableHeight
 
 		xt, yt := r.normalize(x, y)
 
 		// error correction
-		yE1 := float32((point.Y+point.Error-scope.MinY)/yRange) * availableHeight
-		yE2 := float32((point.Y-point.Error-scope.MinY)/yRange) * availableHeight
+		yE1 := float32((point.Y+point.Error-minY)/yRange) * availableHeight
+		yE2 := float32((point.Y-point.Error-minY)/yRange) * availableHeight
 
 		_, e1 := r.normalize(x, yE1)
 		_, e2 := r.normalize(x, yE2)
@@ -69,15 +76,29 @@ func (r *GraphRenderer) DrawGraphLinear(scope *function.Scope, points, iPoints f
 		r.DrawPoint(xt, yt, pointColor)
 	}
 }
+func floorInOrder(num float64, order int) float64 {
+	return math.Floor(num*math.Pow10(-order)) * math.Pow10(order)
+}
+func ceilInOrder(num float64, order int) float64 {
+	return math.Ceil(num*math.Pow10(-order)) * math.Pow10(order)
+}
 
 // draw grid lines and labels for linear scale
 func (r *GraphRenderer) DrawGridLinear(scope *function.Scope) {
+
+	//
+	order := int(math.Floor(math.Log10(math.Abs(scope.MaxY - scope.MinY))))
+	minX := floorInOrder(scope.MinX, order)
+	minY := floorInOrder(scope.MinY, order)
+	maxX := ceilInOrder(scope.MaxX, order)
+	maxY := ceilInOrder(scope.MaxY, order)
+
 	// horizontal grid-lines + y-labels
-	yGridCount := int(r.size.Height / 25)
-	yStep := (scope.MaxY - scope.MinY) / float64(yGridCount)
+	yGridCount := int(math.Ceil((maxY - minY) * math.Pow10(-order))) //TODO make -
+	yStep := (maxY - minY) / float64(yGridCount)
 
 	for i := 0; i <= yGridCount; i++ {
-		value := scope.MinY + yStep*float64(i)
+		value := minY + yStep*float64(i)
 		yPos := r.size.Height - r.margin - float32(i)*float32(r.size.Height-1.5*r.margin)/float32(yGridCount)
 
 		if i > 0 {
@@ -101,7 +122,7 @@ func (r *GraphRenderer) DrawGridLinear(scope *function.Scope) {
 
 	// vertical grid-lines + x-labels
 	xGridCount := int(r.size.Width / 25)
-	xStep := math.Abs(scope.MaxX-scope.MinX) / float64(xGridCount)
+	xStep := math.Abs(maxX-minX) / float64(xGridCount)
 
 	for i := 0; i <= xGridCount; i++ {
 		xPos := r.margin + float32(i)*float32(r.size.Width-1.5*r.margin)/float32(xGridCount)
@@ -112,7 +133,7 @@ func (r *GraphRenderer) DrawGridLinear(scope *function.Scope) {
 
 		// only draw every second label to prevent overlapping
 		if i%4 == 0 {
-			v := scope.MinX + xStep*float64(i)
+			v := minX + xStep*float64(i)
 			text := fmt.Sprintf("%.3f", v)
 			if v < 0.01 {
 				text = fmt.Sprintf("%.1e", v)
